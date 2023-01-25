1ff203f80655114e0962f0b1b4f4aa886056a7b2
K N V PHANI KUMAR <119072808+knvphanikumar@users.noreply.github.com>
2023-01-25 12:51:23 +0530

    change

--- main.tf
+++ main.tf
@@ -1,14 +1,16 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
   access_key = "AKIA4QUKASHDSEKLAZKU"
   secret_key = "Jmg881hHLAuq2IJGn6SdDyutmfM9OZZaBapq5GfQ"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0e6329e222e662a52"
-  instance_type = var.instance_type
+  instance_type = "t2.micro"
+  key_name = "phanikey"
+  availability_zone = "us-east-1d"
   tags= {
-    Name = "raham"
+    Name = "phani-terraform"
   }
 }
 
